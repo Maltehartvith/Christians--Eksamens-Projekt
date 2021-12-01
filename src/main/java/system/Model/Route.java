0c0b443f2958cb9dbb437132648c06caf9684c40
thorminathor4 <thorfa4444@gmail.com>
2021-12-01 14:35:31 +0100

    Glemte at importere ArrayList i Route

--- src/main/java/system/Model/Route.java
+++ src/main/java/system/Model/Route.java
@@ -1,13 +1,14 @@
 package system.Model;
+import java.util.ArrayList;
 
-public class Route {
+public class Route{
     private long routeID;
     private Arraylist<Attraction> attractions = new ArrayList<>();
 
     public Route(){}
     public long getRouteID(){return routeID;}
     public void setRouteID(long tourID){this.routeID=routeID;}
-    public Arraylist<Attraction> getAttractions() {return attractions;}
+    public Arraylist<Attraction> getAttractions(){return attractions;}
     public addAttraction(Attraction attraction){attractions.add(attraction);}
     public removeAttraction(Attraction attraction){attractions.remove(attraction);}
 }
