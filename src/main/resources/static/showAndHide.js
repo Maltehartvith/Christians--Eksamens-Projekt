c2ca7a2ded337c88bae49c5b8c2ec4b893c1d785
Malte <fe12malhar@gmail.com>
2021-12-30 18:41:08 +0100

    Nu kan man også gemme ruter ;)

--- src/main/resources/static/showAndHide.js
+++ src/main/resources/static/showAndHide.js
@@ -26,3 +26,25 @@ function hideAttractions() {
     document.getElementById("attraction-header-index").onclick=showAttractions
     document.getElementById("attraction-content").style.display="none"
 }
+
+function showRoutes() {
+    document.getElementById("route-div").style.height='350px'
+    document.getElementById("routes-header").innerHTML = "Ruter &uarr;<a id=\"question-button\">\n" +
+        "                    <span class=\"d-inline-block\" tabindex=\"0\" data-bs-toggle=\"tooltip\" title=\"Her kan du lave en rute. Når du trykker på knappen,\n" +
+        "så sætter du en usynlig markør, hver gang du trykker på kortet.&#10;Tryk flere gange for at lave flere punkter,&#10;og når du er tilfreds med ruten, kan du trykke på gem rute.\">\n" +
+        "  <button class=\"btn btn-primary\" type=\"button\" style=\"border-radius: 20px; padding: 0px; height: 20px; width: 20px; font-size: 12px; left: 100%\" disabled>?</button>\n" +
+        "</span></a></h2>"
+    document.getElementById("routes-header-index").onclick=hideRoutes
+    document.getElementById("route-content").style.display="block"
+}
+
+function hideRoutes() {
+    document.getElementById("route-div").style.height="92px"
+    document.getElementById("routes-header").innerHTML="Ruter &darr; <a id=\"question-button\">\n" +
+        "                    <span class=\"d-inline-block\" tabindex=\"0\" data-bs-toggle=\"tooltip\" title=\"Her kan du lave en rute. Når du trykker på knappen,\n" +
+        "så sætter du en usynlig markør, hver gang du trykker på kortet.&#10;Tryk flere gange for at lave flere punkter,&#10;og når du er tilfreds med ruten, kan du trykke på gem rute.\">\n" +
+        "  <button class=\"btn btn-primary\" type=\"button\" style=\"border-radius: 20px; padding: 0px; height: 20px; width: 20px; font-size: 12px; left: 100%\" disabled>?</button>\n" +
+        "</span></a></h2>"
+    document.getElementById("routes-header-index").onclick=showRoutes
+    document.getElementById("route-content").style.display="none"
+}
